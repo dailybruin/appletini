a43e69abb73346be0235add7c7d8a3442f74a0c8
Ryan Hansberry <rhansby@gmail.com>
2014-05-19 21:38:52 -0700

    Music: added colorThief. Now grabs primary color of albums and applies it to panes: http://puu.sh/8Ttsi.png

--- music/static/music/js/main.js
+++ music/static/music/js/main.js
@@ -1,4 +1,14 @@
-$(document).ready(function() {
-    // Hello wurld
-});
+var colorThief = new ColorThief();
 
+var recolorBackground = function($album_cover) {
+    var rgb = colorThief.getColor($album_cover),
+        color_string = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+
+    $($album_cover).parent().css('background-color', color_string);
+};
+
+$('.albums').imagesLoaded(function() {
+    Array.prototype.forEach.call($('.album-artwork'), function($a) {
+        recolorBackground($a);
+    });
+});
