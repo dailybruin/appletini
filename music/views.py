298c463349e03cb96c7fbb0e8c6f1116a0e43978
Ryan Hansberry <rhansby@gmail.com>
2014-05-20 20:48:10 -0700

    Music: added star rating to album views (as of yet not completely styled) :star:

--- music/views.py
+++ music/views.py
@@ -12,5 +12,17 @@ class MainView(TemplateView):
         context = super(MainView, self).get_context_data(**kwargs)
         context['albums'] = Album.objects.all()
 
+        # Allow for clean rating star rending in the template:
+        max_rating = 5
+        for album in context['albums']:
+            rating = album.rating
+            if rating < 0:
+                rating = 0
+            elif rating > max_rating:
+                rating = max_rating
+
+            album.rating_range = xrange(rating)
+            album.rating_range_alt = xrange(max_rating - rating)
+
         return context
 
