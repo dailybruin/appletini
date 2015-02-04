3fe129849f2ab19b56aee95a455814b510755d85
anumat <anumat.s@hotmail.com>
2015-02-03 18:27:30 -0800

    diy stuff

--- prime/migrations/0001_initial.py
+++ prime/migrations/0001_initial.py
@@ -22,11 +22,12 @@ class Migration(SchemaMigration):
         db.create_table(u'prime_article', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('issue', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['prime.Issue'], null=True, blank=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=64)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=64)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=128)),
             ('lead_photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
             ('teaser', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('body', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('redirect', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
             ('position', self.gf('django.db.models.fields.PositiveIntegerField')(default=0)),
         ))
         db.send_create_signal(u'prime', ['Article'])
@@ -40,12 +41,56 @@ class Migration(SchemaMigration):
         ))
         db.create_unique(m2m_table_name, ['article_id', 'author_id'])
 
+        # Adding model 'Recipe'
+        db.create_table(u'prime_recipe', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=128)),
+            ('lead_photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('teaser', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('body', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('redirect', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('position', self.gf('django.db.models.fields.PositiveIntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'prime', ['Recipe'])
+
+        # Adding M2M table for field author on 'Recipe'
+        m2m_table_name = db.shorten_name(u'prime_recipe_author')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('recipe', models.ForeignKey(orm[u'prime.recipe'], null=False)),
+            ('author', models.ForeignKey(orm[u'main.author'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['recipe_id', 'author_id'])
+
+        # Adding model 'DIYarticle'
+        db.create_table(u'prime_diyarticle', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=128)),
+            ('lead_photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('teaser', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('body', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('redirect', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('position', self.gf('django.db.models.fields.PositiveIntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'prime', ['DIYarticle'])
+
+        # Adding M2M table for field author on 'DIYarticle'
+        m2m_table_name = db.shorten_name(u'prime_diyarticle_author')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('diyarticle', models.ForeignKey(orm[u'prime.diyarticle'], null=False)),
+            ('author', models.ForeignKey(orm[u'main.author'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['diyarticle_id', 'author_id'])
+
         # Adding model 'Image'
         db.create_table(u'prime_image', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
             ('issue', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['prime.Issue'], null=True, blank=True)),
-            ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.Author'])),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['main.Author'], null=True, blank=True)),
             ('caption', self.gf('django.db.models.fields.TextField')(blank=True)),
         ))
         db.send_create_signal(u'prime', ['Image'])
@@ -69,6 +114,18 @@ class Migration(SchemaMigration):
 
         # Removing M2M table for field author on 'Article'
         db.delete_table(db.shorten_name(u'prime_article_author'))
+
+        # Deleting model 'Recipe'
+        db.delete_table(u'prime_recipe')
+
+        # Removing M2M table for field author on 'Recipe'
+        db.delete_table(db.shorten_name(u'prime_recipe_author'))
+
+        # Deleting model 'DIYarticle'
+        db.delete_table(u'prime_diyarticle')
+
+        # Removing M2M table for field author on 'DIYarticle'
+        db.delete_table(db.shorten_name(u'prime_diyarticle_author'))
 
         # Deleting model 'Image'
         db.delete_table(u'prime_image')
@@ -130,18 +187,31 @@ class Migration(SchemaMigration):
         u'prime.article': {
             'Meta': {'object_name': 'Article'},
             'author': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['main.Author']", 'symmetrical': 'False'}),
-            'body': ('django.db.models.fields.TextField', [], {}),
+            'body': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'issue': ('django.db.models.fields.related.ForeignKey', [], {'default': 'None', 'to': u"orm['prime.Issue']", 'null': 'True', 'blank': 'True'}),
             'lead_photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
             'position': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '64'}),
+            'redirect': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '128'}),
             'teaser': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'prime.diyarticle': {
+            'Meta': {'object_name': 'DIYarticle'},
+            'author': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['main.Author']", 'symmetrical': 'False'}),
+            'body': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lead_photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'position': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'redirect': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '128'}),
+            'teaser': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
         },
         u'prime.image': {
             'Meta': {'object_name': 'Image'},
-            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.Author']"}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['main.Author']", 'null': 'True', 'blank': 'True'}),
             'caption': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
@@ -161,6 +231,18 @@ class Migration(SchemaMigration):
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
             'issue': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['prime.Issue']", 'unique': 'True'}),
             'pdf': ('django.db.models.fields.files.FileField', [], {'max_length': '100'})
+        },
+        u'prime.recipe': {
+            'Meta': {'object_name': 'Recipe'},
+            'author': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['main.Author']", 'symmetrical': 'False'}),
+            'body': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'lead_photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'position': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'redirect': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '128'}),
+            'teaser': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
         }
     }
 
