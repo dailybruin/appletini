5dd548c4a9a613c6c79f5091b03a1e1addfd9cd6
Anumat Srivastava <anumat.s@hotmail.com>
2015-05-18 10:56:06 -0700

    trying to fix repetition of aritcles.

--- prime/views.py
+++ prime/views.py
@@ -86,7 +86,7 @@ class ArticleView(View):
 class LandingView(View):
     def get(self, context):
         current_issue, _ = get_recent_issues()
-        article_list = Article.objects.order_by('issue').reverse()
+        article_list = Article.objects.order_by('issue').reverse().distinct()
         paginator = Paginator(article_list, 4)
         page = self.request.GET.get('page')
         try:
