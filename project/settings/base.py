c91cdc404e179c1120534196a8dcfe0b7a6e2df1
Chang Liu <shampliu@gmail.com>
2015-11-16 04:48:53 -0800

    More settings.py config changes

--- project/settings/base.py
+++ project/settings/base.py
@@ -74,7 +74,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'prime/static/'
 MEDIA_URL = '/media/'
 
 RECIPE_MEDIA_URL = '/uploads/'
@@ -85,7 +85,7 @@ RECIPE_MEDIA_URL = '/uploads/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 
-STATIC_ROOT = BASE_DIR + "/static"
+STATIC_ROOT = BASE_DIR + "/../static"
 MEDIA_ROOT = BASE_DIR + "/../uploads"
 
 
