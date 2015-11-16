3e4a0c43bd13f17bf0b9af6dc320aa172abc47ff
Chang Liu <shampliu@gmail.com>
2015-11-16 04:50:31 -0800

    Round 3

--- project/settings/base.py
+++ project/settings/base.py
@@ -74,7 +74,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = 'prime/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 RECIPE_MEDIA_URL = '/uploads/'
