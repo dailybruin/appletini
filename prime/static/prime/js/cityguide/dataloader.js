6d34c0faaba388616597da9601fa4b2a768ce075
Chang Liu <shampliu@gmail.com>
2015-11-15 19:06:47 -0800

    Map scrolling works

--- prime/static/prime/js/cityguide/dataloader.js
+++ prime/static/prime/js/cityguide/dataloader.js
@@ -1,4 +1,3 @@
-var markers = new Array();
 var currentCard = -1;
 var autoMapScroll = 0;
 var mapMarkers = new Array();
@@ -8,93 +7,46 @@ var currentPinIndex = -1;
 
 // Gets data from Google Spreadsheets
 function addDataToMap(){
-    $.getJSON(dataURL, function(json){
-      
-      var data = {places: json.feed.entry};
-      console.log(data);
-      // console.log(data.places[0]["gsx$address"]);
-      var content = document.getElementById("content")
-      
-      var source = $("#card_template").html(); 
-      var template = Handlebars.compile(source)
-      content.innerHTML = template(data);
-      
-        $.each(data.places, function (index, value){
-        var loc; 
+  $.getJSON(dataURL, function(json){
 
-        var bubble = document.createElement("BUTTON");        
-        var t = document.createTextNode(index + 1);    
+    var data = {places: json.feed.entry};
+    var content = document.getElementById("content")
 
-        if (index == 9) {
-          bubble.className = "content-bubble double-digit"
+    var source = $("#card_template").html(); 
+    var template = Handlebars.compile(source)
+    content.innerHTML = template(data);
+
+    $.each(data.places, function (index, value){
+      var loc; 
+
+      var address = data.places[index]["gsx$place"]["$t"] + ", Los Angeles, CA";
+      gc.geocode( { 'address' : address }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          loc = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
         }
-        else {
-          bubble.className = "content-bubble";
-        }
-        bubble.id = index; 
 
-        if (index == 7 || index == 10 || index == 11) {
-          bubble.innerHTML = '&#x2605;'
-          bubble.className = "content-bubble star"
-        }
-        else {
-          bubble.appendChild(t);
-        }
-        
-        $(bubble).click(function() {
-          clickMarker(bubble.id);
-        })
+        var newURL = normalPinURL; 
 
-        $('#content-nav').append(bubble);
-      
-        var address = data.places[index]["gsx$place"]["$t"] + ",Los Angeles, CA";
-        gc.geocode( { 'address' : address}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            loc = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-            console.log(loc);
-          }
-
-          var newURL;
-          // if (index == 7 || index == 10 || index == 11) {
-          //   newURL = starURL;
-          // }
-          // else {
-            newURL = normalPinURL;
-          // }
-
-          markers[markers.length] = new google.maps.Marker({
-            position: loc,
-            map: map,
-            draggable: false,
-            // title: "0",
-            animation: google.maps.Animation.DROP,
-            icon: newURL
-          });
-
-          var markerIndex = markers.length-1;
-          google.maps.event.addListener(markers[markerIndex], 'click', function() {
-            clickMarker(markerIndex);
-          });
-        });
-      });
-
-      // $("#content").append(cardTemp({apidata: data}));
-      $.each(data.places, function (index, value){
         mapMarkers[mapMarkers.length] = new google.maps.Marker({
-          position: gc.geocode( { 'address' : 'University of California, Los Angeles, CA' }),
+          position: loc,
           map: map,
           draggable: false,
           animation: google.maps.Animation.DROP,
-          title: value["title"],
-          icon: normalPinURL
-      });
+          icon: newURL
+        });
 
-      var markerIndex = mapMarkers.length-1;
-      // google.maps.event.addListener(mapMarkers[markerIndex], 'click', function() {
-      //   clickPin(markerIndex);
-      // });
+        var markerIndex = mapMarkers.length-1;
+        google.maps.event.addListener(mapMarkers[markerIndex], 'click', function() {
+          clickPin(markerIndex);
+        });
+
+
+
+      // $("#content").append(cardTemp({apidata: data}));
+
 
       var cardID = '#card-' + index;
+
       $(window).bind('scroll', function() {
         if(currentCard > index || autoMapScroll != 0)
           return;
@@ -117,11 +69,12 @@ function addDataToMap(){
         }
       });
     });
+});
 
-    // Pan to first item at start
-    panMapTo(0, true);
-    currentPinIndex = -1;
-  });
+});
+  // Pan to first item at start
+  panMapTo(0, true);
+  currentPinIndex = -1;
 }
 
 function clickPin(markerIndex)
